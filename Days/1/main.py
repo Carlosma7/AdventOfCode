a27a36218f2f13202a10ed1dba28c551f68af5cb
Carlos Morales Aguilera <carlos7ma@gmail.com>
2021-12-11 01:59:34 +0100

    Format day 1

--- Days/1/main.py
+++ Days/1/main.py
@@ -5,14 +5,12 @@ def measurement(lst):
 	"""Counts the increases produced in a list of measurements."""
 	return [int(lst[i]) > int(lst[i - 1]) for i in range(1, len(lst))].count(True)
 
-
 # Consider sums of a three-measurement sliding window. 
 # How many sums are larger than the previous sum?
 def three_measurement(lst):
 	"""Counts the increases produced from each sum of three measurements."""
 	three_list = [int(lst[i - 2]) + int(lst[i - 1]) + int(lst[i]) for i in range(2, len(lst))]
 	return measurement(three_list)
-
 
 if __name__ == "__main__":
 	# Get path from actual dirname
